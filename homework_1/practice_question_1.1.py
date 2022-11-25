1db2b3684bce1eaf64b395b3419c6072064ce02b
AlperHuseyn <alperhuseyindogan@gmail.com>
2022-11-25 17:05:22 +0300

    Solve first practice question my own way.

---
+++ homework_1/practice_question_1.1.py
@@ -0,0 +1,16 @@
+""" My first attempt to solve the first practice question """
+
+l1 = [10, 'ali', 'veli', 20, 'selami']
+l2 = ['selami', 20, 'veli', 'ali', 10]
+
+# temp = l1  # temp for not losing l1
+# l1 = l1[::-1]  # Reverse l1
+
+if l1[::-1] == l2:  # if l1 == l2:
+    print('True')
+else:
+    print('False')
+
+# l1 = temp
+
+# Commented out the unnecessary lines and corrected the if condition
