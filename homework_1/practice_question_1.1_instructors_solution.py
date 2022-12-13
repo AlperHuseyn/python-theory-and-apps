f5eac0f52cfda40642996a6af3784a6fe4767778
AlperHuseyn <alperhuseyindogan@gmail.com>
2022-12-13 14:05:27 +0300

    Rename the file as instructor's solution and add his solution.

---
+++ homework_1/practice_question_1.1_instructors_solution.py
@@ -0,0 +1,8 @@
+""" Instructor's solution for the first practice question """
+
+l1 = ['Alper', 10, 'ali', 'veli', 20, 'selami']
+l2 = ['selami', 20, 'veli', 'ali', 10, 'Alper']
+
+result = l1 == l2[::-1]
+print(result)
+
