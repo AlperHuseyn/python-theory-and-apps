8696687dcab83f2e7a90edc9ab0963dc1216c3bd
AlperHuseyn <alperhuseyindogan@gmail.com>
2022-12-15 13:07:34 +0300

    Add another instructor solution for practice question #3.

--- homework_1/practice_question_1.3_instuctors_solution.py
+++ homework_1/practice_question_1.3_instuctors_solution.py
@@ -1,8 +1,14 @@
 """ Instructor's solutions for n + nn + nnn + nnnn pattern """
 
+"""
 n = int(input('Please type single digit int value: '))
 
-# 1st way
+# 1st way:
 rs = n + n * 11 + n * 111 + n * 1111
 print(rs)
+"""
 
+# 2nd way: Using str typ casting and repitition
+n = input('Please type single digit int value: ')
+rs = int(n) + int(n * 2) + int(n * 3) + int(n * 4)
+print(rs)
