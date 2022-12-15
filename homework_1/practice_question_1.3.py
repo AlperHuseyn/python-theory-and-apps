9943c557089ae5072d50819dea22ed0bb5ee0a13
AlperHuseyn <alperhuseyindogan@gmail.com>
2022-12-15 13:02:55 +0300

    Correct the solution for practice question #3.

--- homework_1/practice_question_1.3.py
+++ homework_1/practice_question_1.3.py
@@ -1,4 +1,5 @@
 """ Printing n + nn + nnn + nnnn pattern, my solution """
 
 n = int(input('Please type single digit int value: '))
-print(f'{n} + {n * 10 + n} + {n * 100 + n * 10 + n} + {n * 1000 + n * 100 + n * 10 + n}')
+print(n + n * 11 + n * 111 + n * 1111)
+
