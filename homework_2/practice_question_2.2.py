e01e921b9bbe8102b7e0d693f46a88b9a38b9946
AlperHuseyn <alperhuseyindogan@gmail.com>
2022-12-18 17:09:18 +0300

    Add my solution for practice question #2.2.

---
+++ homework_2/practice_question_2.2.py
@@ -0,0 +1,14 @@
+""" My approach to practice question #2.2 """
+
+int_val = int(input('Please enter numerical text of type int: '))
+
+dct = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
+
+digits = []
+while int_val:
+    digits.append(dct[int_val % 10])
+    int_val //= 10
+
+digits.reverse()
+to_str = ''.join(digits)
+print(to_str, type(to_str))
