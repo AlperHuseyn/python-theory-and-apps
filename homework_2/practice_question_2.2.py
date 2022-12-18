7f44694ac18c5904914574429df4913929e06e13
AlperHuseyn <alperhuseyindogan@gmail.com>
2022-12-18 17:43:54 +0300

    Change the text in input(...) built-in function line #3.

--- homework_2/practice_question_2.2.py
+++ homework_2/practice_question_2.2.py
@@ -1,6 +1,6 @@
 """ My approach to practice question #2.2 """
 
-int_val = int(input('Please enter numerical text of type int: '))
+int_val = int(input('Please enter an integer: '))
 
 dct = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
 
