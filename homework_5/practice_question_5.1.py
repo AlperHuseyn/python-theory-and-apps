143d09720b757d801ef82dcf2cfda4feb79c0169
AlperHuseyn <alperhuseyindogan@gmail.com>
2023-01-08 09:59:26 +0300

    Correct point_values dict.

--- homework_5/practice_question_5.1.py
+++ homework_5/practice_question_5.1.py
@@ -22,17 +22,16 @@ def distribute_deck(deck):
 
 
 def sort_cards(cards):
-    point_values = {
-        'Spades-2': 0, 'Spades-3': 1, 'Spades-4': 2, 'Spades-5': 3, 'Spades-6': 4, 'Spades-7': 5, 'Spades-8': 6,
-        'Spades-9': 7, 'Spades-10': 8, 'Spades-Jack': 9, 'Spades-Queen': 10, 'Spades-King': 11, 'Spades-Ace': 12,
-        'Hearts-2': 13, 'Hearts-3': 14, 'Hearts-4': 15, 'Hearts-5': 16, 'Hearts-6': 17, 'Hearts-7': 18,
-        'Hearts-8': 19, 'Hearts-9': 20, 'Hearts-10': 21, 'Hearts-Jack': 22, 'Hearts-Queen': 23, 'Hearts-King': 24,
-        'Hearts-Ace': 25, 'Diamonds-2': 26, 'Diamonds-3': 27, 'Diamonds-4': 28, 'Diamonds-5': 29,
-        'Diamonds-6': 30, 'Diamonds-7': 31, 'Diamonds-8': 32, 'Diamonds-9': 33, 'Diamonds-10': 34,
-        'Diamonds-Jack': 35, 'Diamonds-Queen': 36, 'Diamonds-King': 37, 'Diamonds-Ace': 38, 'Clubs-2': 39,
-        'Clubs-3': 40, 'Clubs-4': 41, 'Clubs-5': 42, 'Clubs-6': 43, 'Clubs-7': 44, 'Clubs-8': 45, 'Clubs-9': 46,
-        'Clubs-10': 47, 'Clubs-Jack': 48, 'Clubs-Queen': 49, 'Clubs-King': 50, 'Clubs-Ace': 51
-    }
+    point_values = {'Spades-2': 0, 'Hearts-2': 1, 'Diamonds-2': 2, 'Clubs-2': 3, 'Spades-3': 4, 'Hearts-3': 5,
+                    'Diamonds-3': 6, 'Clubs-3': 7, 'Spades-4': 8, 'Hearts-4': 9, 'Diamonds-4': 10, 'Clubs-4': 11,
+                    'Spades-5': 12, 'Hearts-5': 13, 'Diamonds-5': 14, 'Clubs-5': 15, 'Spades-6': 16, 'Hearts-6': 17,
+                    'Diamonds-6': 18, 'Clubs-6': 19, 'Spades-7': 20, 'Hearts-7': 21, 'Diamonds-7': 22, 'Clubs-7': 23,
+                    'Spades-8': 24, 'Hearts-8': 25, 'Diamonds-8': 26, 'Clubs-8': 27, 'Spades-9': 28, 'Hearts-9': 29,
+                    'Diamonds-9': 30, 'Clubs-9': 31, 'Spades-10': 32, 'Hearts-10': 33, 'Diamonds-10': 34,
+                    'Clubs-10': 35, 'Spades-Jack': 36, 'Hearts-Jack': 37, 'Diamonds-Jack': 38, 'Clubs-Jack': 39,
+                    'Spades-Queen': 40, 'Hearts-Queen': 41, 'Diamonds-Queen': 42, 'Clubs-Queen': 43, 'Spades-King': 44,
+                    'Hearts-King': 45, 'Diamonds-King': 46, 'Clubs-King': 47, 'Spades-Ace': 48, 'Hearts-Ace': 49,
+                    'Diamonds-Ace': 50, 'Clubs-Ace': 51}
 
     rev_point_vals = {}
     for key, value in point_values.items():
