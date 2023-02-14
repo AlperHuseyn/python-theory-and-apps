dad343bfc006420abbe70f27d5125e354ea2a2e9
AlperHuseyn <alperhuseyindogan@gmail.com>
2023-02-14 19:14:43 +0300

    Correct practice question 10.2, disp_last_lines, seek and tell methods.

--- homework_10/practice_question.10.2.py
+++ homework_10/practice_question.10.2.py
@@ -4,15 +4,19 @@ import os
 
 
 def disp_last_lines(path, number_of_lines):
-    f = open(path, 'rb')
-    f.seek(-number_of_lines, 2)
-    print(f.read().decode('utf-8'), end='')
+    f = open(path, 'r', encoding='utf-8')
+    f.seek(0, 2)  # seek @ EOF
+    f_size = f.tell()  # Get size
+    f.seek(max(f_size - 1024, 0))  # Set pos @ last n characters
+    lines = f.readlines()
+    for line in lines[-number_of_lines:]:
+        print(line, end='')
     f.close()
 
 
 # Pycharm sets current working directory where the project is created,
 # so getting the root path via os.getcwd() and adding file name to it works for me
 p = f'{os.getcwd()}' + r"\Nature's Fury- Surviving Storms and Volcanic Eruptions.txt"
-n_lines = 1024
+n_lines = 3
 
 disp_last_lines(p, n_lines)
