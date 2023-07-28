7f2b1ebf216be41f293489cbaa75280ca4aed52c
AlperHuseyn <alperdogan@hotmail.com>
2023-07-28 19:11:47 +0300

    Update student-notes.py

--- apps/homework_02/student-notes.py
+++ apps/homework_02/student-notes.py
@@ -1,3 +1,5 @@
+from io import BytesIO
+from PIL import Image
 import os
 import sqlite3
 import sys
@@ -13,12 +15,13 @@ def generate_tables(cursor):
         
         CREATE TABLE IF NOT EXISTS course(
             course_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            course_name VARCHAR(32) UNIQUE,
+            course_name VARCHAR(32),
             course_week_hours INTEGER
             );
         
         CREATE TABLE IF NOT EXISTS grade(
-            student_no INTEGER PRIMARY KEY,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            student_no INTEGER,
             course_name VARCHAR(32),
             course_exam_no INTEGER,
             course_grade INTEGER,
@@ -83,7 +86,21 @@ def grade_entry(cursor):
     try:
         entry = input("Enter the student no, the course name, exam number, and grade in order: ").strip()
         student_no, course_name, course_exam_no, course_grade = entry.split(',')
-                
+        
+        # Check if there are existing grades for the same student and course
+        cursor.execute("""
+                       SELECT MAX(course_exam_no)
+                       FROM grade
+                       WHERE student_no = ? AND course_name = ?;
+                       """, (int(student_no), course_name.strip()))
+        
+        last_exam_no = cursor.fetchone()[0]
+        
+        if last_exam_no is not None:
+            # If there are existing grades, increment the exam number for the new grade
+            course_exam_no = int(last_exam_no) + 1
+        
+        # Insert a new grade entry
         cursor.execute("""INSERT INTO grade(
             student_no,
             course_name,
@@ -109,10 +126,69 @@ def grade_entry(cursor):
 
 def student_query(cursor):
     try:
-        student_no = int(input("Enter the student's number: "))
-                
+        student_no = input("Enter the student's number (press ENTER key for all students): ")
+        
+        # ENTER key press, all students
         if not student_no:
-            pass
+            cursor.execute("""
+                           SELECT student_name, student_no
+                           FROM student;
+                           """)
+        else:
+            student_no = int(student_no)
+            cursor.execute("""
+                           SELECT student_name, student_no
+                           FROM student
+                           WHERE  student_no = ?;                           
+                           """, (student_no, ))
+        
+        students = cursor.fetchall()
+        
+        if not students:
+            print('No student(s) found.')
+            return
+        
+        for student in students:
+            student_name, student_no = student
+            print(f'\nName: {student_name}')
+            print(f'Number: {student_no}')
+            
+            cursor.execute("""
+                           SELECT course_name, course_grade
+                           FROM grade
+                           WHERE student_no = ?;
+                           """, (student_no, ))
+        
+            grades = cursor.fetchall()
+            
+            if grades:
+                # ...
+            
+                print('\nWeighted Average Grade: {avg:.2f}')  # f ekle
+            else:
+                print('No grades found.')
+                
+            print('\nPhoto:')
+            
+            # Fetch the student's photo BLOB from the database
+            cursor.execute("""
+                           SELECT student_photo
+                           FROM student
+                           WHERE student_no = ?
+                           """, (student_no, ))
+            photos = cursor.fetchall()
+            
+            for photo in photos:
+                if photo and photo[0]:
+                    # Create an image object from the BLOB using PIL
+                    img = Image.open(BytesIO(photo[0]))
+                    
+                    # Display the image
+                    img.show()
+                else:
+                    print('No photo found.\n')
+        
+            print('-' * 30)
 
     except sqlite3.Error as err:
         print(f"Record(s) hasn't been listed **[{err}]**")
